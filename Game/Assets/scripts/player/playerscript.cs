74c2d8ab35830289606177c0e886b860cf26fb49
Will Yelton <willyelton@Wills-MBP.cinci.rr.com>
2016-01-31 16:21:34 -0500

    test scene

---
+++ Game/Assets/scripts/player/playerscript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerscript : MonoBehaviour {
+
+	Animator anim;
+	int jumpHash = Animator.StringToHash("Jump");
+	int runStateHash = Animator.StringToHash("Base Layer.Run");
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+
+	void Update () {
+		float move = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", move);
+
+
+		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo (0);
+		if (Input.GetKeyDown (KeyCode.Space) && stateInfo.nameHash == runStateHash) {
+			anim.SetTrigger (jumpHash);
+		}
+	}
+}
